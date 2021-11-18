f83c63a0a5414b540fb997711c1f7d8abbf2caf5
daniellybx <danielly.xavier@outlook.com>
2021-11-18 17:11:18 -0300

    Ajustando scripts do cap. 02.

--- scripts/02_variaveis_apresentacao_dados.R
+++ scripts/02_variaveis_apresentacao_dados.R
@@ -4,13 +4,13 @@
 #install.packages("summarytools")
 #install.packages("reshape2")
 #install.packages("tidyverse")
-#install.packages("GGally")
+#install.packages("htmlTable")
 
 library(summarytools)
 library(descr)
 library(reshape2)
 library(tidyverse)
-library(GGally)
+library(htmlTable)
 
 # Estatísticas descritivas: variáveis e apresentação de dados
 
@@ -86,6 +86,37 @@ tab04 = dcast(tabagista ~ estado_saude, data = tab04)
 
 descr::CrossTable(exemplo$tabagista, exemplo$estado_saude)
 gmodels::CrossTable(exemplo$tabagista, exemplo$estado_saude)
+
+#### Criando uma tabela de frequência em formato de matrix
+mx <- matrix(1:16, ncol = 2, nrow = 8)
+
+rownames(mx) <- c("M", "F", "1-10", "11-20", "21-30", "31-40", "41-50", "50+")
+colnames(mx) <- c("Frequencia", "%")
+
+#### Formatando a tabela em html com subgrupos em linhas
+htmlTable(mx,
+          rgroup = c("Sexo", "Faixa Etaria"),
+          n.rgroup = c(2,4)
+)
+
+#### Criando uma tabela cruzada em formato de matrix
+mx2 <- matrix(1:48, ncol = 6, nrow = 8)
+
+rownames(mx2) = paste0("Linha", 1:8)     
+colnames(mx2) = paste0("Col", 1:6)  
+
+#### Formatando a tabela em html com subgrupos em colunas
+htmlTable(mx2,
+          cgroup = c("Sexo", "Faixa Etaria"),
+          n.cgroup = c(2,4)
+)
+
+htmlTable(mx2,
+          cgroup = rbind(c("", "Variaveis", NA),
+                         c("", "Sexo", "Faixa Etaria")),
+          n.cgroup = rbind(c(1,2,NA),
+                           c(2,2,2))
+)
 
 #### Gráfico de dispersão - relação entre duas variáveis quantitativas contínuas
 
