7a85c173d8a71a4cf1afa769436a35d572c0f7e7
daniellybx <danielly.xavier@outlook.com>
2021-12-01 18:45:19 -0300

    Incluindo script de medidas resumo.

---
+++ scripts/03_medidas_resumo.R
@@ -0,0 +1,13 @@
+#carregando os pacotes necessários
+
+#install.packages("descr")
+#install.packages("summarytools")
+#install.packages("reshape2")
+#install.packages("tidyverse")
+#install.packages("htmlTable")
+
+library(summarytools)
+library(descr)
+library(reshape2)
+library(tidyverse)
+library(htmlTable)
